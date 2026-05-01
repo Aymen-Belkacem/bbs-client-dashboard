23620c89513163e52b0c0f880ebcf1b06aff0547
Aymen <aymen.belkacem@gmail.com>
2026-05-01 18:35:45 +0200

    Update API_BASE_URL to use environment variable

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://web-api-3-bbs-d5hcb6fjbrb7cdau.italynorth-01.azurewebsites.net";
+const API_BASE_URL = window.env.API_BASE_URL;
 
 let chartDay = null;
 let chartFood = null;
